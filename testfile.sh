9cf2637e7c9452797c946fc44d193c9aafd30db8
nagarjun-oss <nishuarjun1997@gmail.com>
2025-09-24 11:22:53 +0530

    Create testfile.sh

---
+++ testfile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Navigate to the directory containing your code and tests
+cd /path/to/your/project
+
+# Run your test commands
+# For a Node.js project, it might be:
+npm test
+
+# For a Python project, it might be:
+python -m unittest discover
+
+# For a simple shell script test, it could be:
+./my_script.sh
